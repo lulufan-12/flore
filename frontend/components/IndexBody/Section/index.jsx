99c312eb0841578d69ee463b575dfd3e793f9842
Luis Martins <lulufan.12@gmail.com>
2021-03-16 13:57:27 -0300

    Main area done with cards

--- frontend/components/IndexBody/Section/index.jsx
+++ frontend/components/IndexBody/Section/index.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
+import Card from '../../UI/Card'
 import classes from './classes.module.css'
 
 const section = (props) => {
     const list_items = props.items.map(item => {
         return (
-            <p>
-                Card {item.id} aqui
-            </p>
+            <Card product={item} key={item.id} />
         )
     })
     return (
         <section className={classes.Section}>
-            <h2 className={classes.SectionTitle}>
-                {props.title}
-            </h2>
+            <h1 className={classes.SectionTitle}>{props.title}</h1>
             <div className={classes.CardsHolder}>
                 {list_items}
             </div>
+            
         </section>
     )
 }
--- frontend/components/IndexBody/index.jsx
+++ frontend/components/IndexBody/index.jsx
@@ -4,14 +4,22 @@ import Section from './Section'
 import classes from './classes.module.css'
 
 const indexbody = () => {
-    const items = [{id: 1}, {id: 2}, {id: 3}]
+    const items = [
+        {id: 1, title: "Card 1", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"},
+        {id: 2, title: "Card 2", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"},
+        {id: 3, title: "Card 3", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"},
+        {id: 4, title: "Card 4", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"},
+        {id: 5, title: "Card 5", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"},
+        {id: 6, title: "Card 5", price: "12.79", priceDivided: "3x de 5.00", path: "assets/aneis.jpg"}
+    ]
     return (
         <main className={classes.Main}>
             <h1 className={classes.Subtitle}>Faça o que te faz <span className={classes.SubtitleEmphasis}>Florê</span></h1>
             <Carousel />
             <img src="assets/banner-fino.jpg" alt="Formas de Pagamento" className={classes.Img} />
             <Section title="LANÇAMENTOS" items={items} />
-            
+            <Section title="PROMOÇÕES" items={items} />
+            <Section title="RECOMENDADOS" items={items} />
         </main>
     )
 }
